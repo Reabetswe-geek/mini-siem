d90f575071af1e9db4b938fec7842d669de4cc0a
Reabetswe <putukacaiphus@gmail.com>
2026-04-22 17:14:56 +0200

    Added username to SIEM TOOL

--- siem.py
+++ siem.py
@@ -6,6 +6,10 @@ def extract_ip(line):
     match = re.search(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', line)
     return match.group() if match else  None
 
+def extract_user(line):
+    match = re.search(r'for (?:invalid user )?(\w+)', line)
+    return match.group(1) if match else "unknown"
+
 def extract_time(line):
     try:
         timestamp_str = line.split()[0]
@@ -23,18 +27,19 @@ def analyze_logs(file_path, windows_minutes=10, threshold=3):
 
             ip = extract_ip(line)
             timestamp = extract_time(line)
+            user = extract_user(line)
 
             if not ip or not timestamp:
                 continue
 
-            failed_logins[ip].append(timestamp)
+            failed_logins[(ip, user)].append(timestamp)
 
     print("\n==== SIEM REPORT ====\n")
     print(f"Window: {windows_minutes} minutes | Threshold: {threshold} attempts\n")
 
     alerts = []
 
-    for ip,  times in failed_logins.items():
+    for ( ip, user ), times in failed_logins.items():
         times.sort()
 
         for i in range(len(times)):
@@ -44,18 +49,18 @@ def analyze_logs(file_path, windows_minutes=10, threshold=3):
             count_in_window = sum(1 for t in times[i:] if window_start <= t <= window_end)
 
             if count_in_window >= threshold:
-                alerts.append((ip, count_in_window, window_start,window_end))
+                alerts.append((ip, user, count_in_window, window_start,window_end))
                 break
 
     if not alerts:
         print("No alerts triggered.")
     else:
-        for ip, count,start, end in sorted(alerts, key=lambda x: x[1], reverse=True):
-            print(f"ALERT: {ip} -> {count} failed attempts")
+        for ip, user, count,start, end in sorted(alerts, key=lambda x: x[2], reverse=True):
+            print(f"ALERT: {ip} -> {count} failed attempts for user 'user'")
             print(f"Window: {start.strftime('%H:%M:%S')} to {end.strftime('%H:%M:%S')}\n")
 
 def main():
-    file_path = input("Entet log file path: ")
+    file_path = input("Enter log file path: ")
     analyze_logs(file_path)
 
 if __name__ == "__main__":
